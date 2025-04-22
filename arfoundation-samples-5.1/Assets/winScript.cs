d9c3b78cd040334d6787e8d043c25439b5dd0bfc
Linjiajie2002 <92087408+Linjiajie2002@users.noreply.github.com>
2025-04-22 18:05:09 +0200

    JJL- FADE OUT

--- arfoundation-samples-5.1/Assets/winScript.cs
+++ arfoundation-samples-5.1/Assets/winScript.cs
@@ -6,22 +6,44 @@ using UnityEngine.SceneManagement;
 public class winScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    public CanvasGroup canvasGroup;      
+    public float fadeDuration = 5f;       // Tiempo de fadeOut
     void Start()
     {
-        StartCoroutine(LoadMainMenuAfterDelay());
+        StartCoroutine(FadeOutAndSwitch());
 
 
     }
 
-    IEnumerator LoadMainMenuAfterDelay()
-    {
-        yield return new WaitForSeconds(5f); // Espera 5 segundos
-        SceneManager.LoadScene("MainMenu");
-    }
+    //IEnumerator LoadMainMenuAfterDelay()
+    //{
+    //    yield return new WaitForSeconds(5f); // Espera 5 segundos
+    //    SceneManager.LoadScene("MainMenu");
+    //}
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    IEnumerator FadeOutAndSwitch()
+    {
+        float time = 0f;
+        canvasGroup.alpha = 0f; 
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            canvasGroup.alpha = time / fadeDuration; 
+            yield return null;
+        }
+
+        canvasGroup.alpha = 1f; 
+        yield return new WaitForSeconds(0.5f); 
+
+        SceneManager.LoadScene("MainMenu");
     }
 }
